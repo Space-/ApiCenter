ee04536ad3e98c63c408f7f998887a2d3b3cb0ea
Bruce <gkm85663@gmail.com>
2020-03-04 01:00:26 +0800

    Extend Swagger UI

--- ApiCenter/Startup.cs
+++ ApiCenter/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -21,9 +22,31 @@ namespace ApiCenter
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            // services.AddSwaggerGen(c =>
+            // {
+            //     c.SwaggerDoc("v1", new OpenApiInfo() { Title = "MyAPI", Version = "v1" });
+            // });
+
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo() { Title = "MyAPI", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Version = "v1",
+                    Title = "TODO API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact()
+                    {
+                        Name = "Shayne Boyer",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/spboyer")
+                    },
+                    License = new OpenApiLicense()
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license")
+                    }
+                });
             });
         }
 
